f3bd9a126ba13e9ddc6a3c5f20d481ddb3e1bf28
Robert Lyons <rglyons@ucsc.edu>
2017-02-07 17:43:11 -0800

    added models

---
+++ server/models/entry.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Entry = sequelize.define('Entry', {
+    humidity: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    temperature:  {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    sunlight:  {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    moisture:  {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Entry.belongsTo(models.Sensor, {
+          foreignKey: 'sensorId',
+          //onDelete: 'CASCADE',
+        });
+      }
+    }
+  });
+  return Entry;
+};
---
+++ server/models/sensor.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Sensor = sequelize.define('Sensor', {
+    ipaddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Sensor.hasMany(models.Entry, {
+          foreignKey: 'sensorId',
+          as: 'entries',
+        };)
+      }
+    }
+  });
+  return Sensor;
+};
