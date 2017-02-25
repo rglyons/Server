e57e131ed252e9e408398dee65f7d046118058f0
Robert Lyons <rglyons@ucsc.edu>
2017-02-24 21:25:57 -0800

    trying to fix SSL connection

--- server/models/index.js
+++ server/models/index.js
@@ -9,7 +9,12 @@ var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: true
+    }
+  });
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
