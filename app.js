515e7bfdbccf4f46a5107a874caf9c4c52e69bcd
ttagawa <tylertagawa@gmail.com>
2017-04-24 02:02:18 -0700

    attempt at getting cors to work.

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ global.Models = require("./server/models");
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 // Set up the express app
 const app = express();
 const port = process.env.PORT;
-
+app.use(cors());
 // Log requests to the console.
 app.use(logger('dev'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'))
