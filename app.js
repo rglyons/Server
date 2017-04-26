fd0a7259b4298cfaa2e142f03f6fa4cb7bad81fb
Raul Lopez-Luna <rlopezlu@Rauls-MacBook-Pro.local>
2017-04-26 02:35:39 -0700

    initial setup of passport with the server

--- app.js
+++ app.js
@@ -3,6 +3,44 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+//passport and local Strategy
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+//setup passport local Strategy
+passport.use(new Strategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) {
+        console.log("err ", err);
+        return done(err);
+      } if (!user) {
+        console.log("no user, !user");
+        return done(null, false, { message: 'Incorrect username.' });
+      } if (!user.validPassword(password)) {
+        console.log("wrong password");
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      console.log("success, found a user");
+      return done(null, user);
+    });
+  }
+));
+
+//serialize and deserialize users with passport
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+
+
+
 // Set up the express app
 const app = express();
 const port = process.env.PORT;
@@ -15,11 +53,29 @@ app.use("/static", express.static(__dirname + '/static'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(require('express-session')({ secret: 'keyboard cat'}));
+// Initialize Passport and restore authentication state, from the session
+app.use(passport.initialize());
+app.use(passport.session());
+
+var isAuthenticated = function (req, res, next) {
+	if (req.isAuthenticated())
+		return next();
+	res.redirect('/');
+}
+
+//login using passport-local
+app.post('/',
+  passport.authenticate('local', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
 //webapp hosting stuff
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/login.html');
 });
-app.get('/index.html',(req,res) =>{
+app.get('/index.html', isAuthenticated, (req,res) =>{
   res.sendFile(__dirname + '/index.html');
 });
 // Require our routes into the application.
