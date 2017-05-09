70a9a177a85cc1e5a1d70622c930c9004e710afb
Robert Lyons <rglyons@ucsc.edu>
2017-05-08 19:28:44 -0700

    battery field added to entry; schema/API functions updated

--- server/controllers/entry.js
+++ server/controllers/entry.js
@@ -8,6 +8,7 @@ module.exports = {
         sunlight: req.body.sunlight,
         temperature: req.body.temperature,
         moisture: req.body.moisture,
+        battery: req.body.battery,
         sensorId: req.params.sid,
       })
       .then(entry => res.status(201).send(entry))
