554a6ac529f92c40af6761ac8ae7e93f0129354f
Robert Lyons <rglyons@ucsc.edu>
2017-07-12 21:02:47 -0700

    updated comments

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,18 +12,18 @@ module.exports = (app) => {
   app.post('/api/nodes', auth.validate, nodesController.create); //create node under user
   app.get('/api/nodes/all', nodesController.list); // list all nodes
   app.get('/api/node/:nid', nodesController.getNodeById); // retrieve node + readings
-  app.post('/api/nodes/:nid/readings', readingsController.create); // create  for sensor
-  app.put('/api/nodes/:nid', auth.validate, nodesController.update); // update sensor fields
-  app.delete('/api/nodes/:nid', auth.validate, nodesController.destroy); // delete sensor
+  app.post('/api/nodes/:nid/readings', readingsController.create); // create reading for node
+  app.put('/api/nodes/:nid', auth.validate, nodesController.update); // update node fields
+  app.delete('/api/nodes/:nid', auth.validate, nodesController.destroy); // delete node
 
   // users
   app.post('/api/users/login', auth.login); // login user
   app.post('/api/users', usersController.create); //create user
   app.put('/api/users/update', auth.validate, usersController.update); // update user fields
   app.put('/api/users/token', auth.validate, usersController.generateApiToken); // generate new api token for existing user
-  app.post('/api/users/getuser', auth.validate, usersController.getUser); // retrieve user + sensors
+  app.post('/api/users/getuser', auth.validate, usersController.getUser); // retrieve user + nodes
   app.post('/api/users/node_readings',
-            auth.validate, nodesController.getLatestNodeReadingsForUser); // retrieve latest reading for each of a user's sensors
+            auth.validate, nodesController.getLatestNodeReadingsForUser); // retrieve latest reading for each of a user's nodes
   app.delete('/api/users/delete', auth.validate, usersController.destroy); // delete user
 
   //other methods
