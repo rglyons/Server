d648a7057404047b9867bf7959afc0692c645135
Robert Lyons <rglyons@ucsc.edu>
2017-08-11 14:06:26 -0700

    tests written in different files and aggregated at runtime with npm run test

--- test/tests.js
+++ test/tests.js
@@ -1,26 +1,37 @@
 // Calls all test modules in sequence
 // to avoid race conditions
 
-describe('user.js', function () {
-  require('./user.js')
-});
-
-describe('node_and_reading.js', function () {
-  require('./node_and_reading.js')
-});
-
-describe('other.js', function () {
-  require('./other.js')
-});
-
-describe('depr_user.js', function () {
-  require('./depr_user.js')
-});
-
-describe('depr_node_and_reading.js', function () {
-  require('./depr_node_and_reading.js')
-});
-
-describe('depr_other.js', function () {
-  require('./depr_other.js')
+describe('test suite', () => {
+  before(() => {})
+  after(() => {})
+  beforeEach(() => {})
+  afterEach(() => {})
+  it('run tests in user.js', (done) => {
+    require('./user.js')
+    done()
+  })
+  it('run tests in node_and_reading.js', (done) => {
+    require('./node_and_reading.js')
+    done()
+  })
+  it('run tests in other.js', (done) => {
+    require('./other.js')
+    done()
+  })
+  it('run tests in notification.js', (done) => {
+    require('./notification.js')
+    done()
+  })
+  it('run tests in depr_user.js', (done) => {
+    require('./depr_user.js')
+    done()
+  })
+  it('run tests in depr_node_and_reading.js', (done) => {
+    require('./depr_node_and_reading.js')
+    done()
+  })
+  it('run tests in depr_other.js', (done) => {
+    require('./depr_other.js')
+    done()
+  })
 });
