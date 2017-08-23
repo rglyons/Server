630652a4754a0d02d70da72c366ff2214ebe920d
Ewocker <chockerlin@gmail.com>
2017-08-22 23:11:35 -0700

    feat(frontend/, auth/auth.js, app.js): finish login feature, clean up unused file.
    Add validateLogin in auth.js, change root route in app.js. Comment out unused and confusing route
    for getting login.html and index.html. Clean up base level unused file such as login/, About/,
    index.html (these are old files that is not being used).

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -1,81 +1,95 @@
-const User = require('../models').User;
-const Node = require('../models').Node;
-const Notification = require('../models').Notification;
+const User = require('../models').User
+const Node = require('../models').Node
+const Notification = require('../models').Notification
 
 module.exports = {
-  
-  login(req, res) {
+
+  login (req, res) {
     return User
-      .findOne( { where: 
-        { 
-          username: req.body.username,
-          password: req.body.password,          
+      .findOne({ where:
+      {
+        username: req.body.username,
+        password: req.body.password
+      },
+        include:
+        [{
+          model: Node,
+          as: 'nodes'
         },
-        include: 
-          [{
-            model: Node,
-            as: 'nodes',
+        {
+          model: Notification,
+          as: 'notifications',
+          where: {
+            dismissed: false
           },
-          {
-            model: Notification,
-            as: 'notifications',
-            where: {
-              dismissed: false
-            },
-            required: false // fix issue where above where clause caused
-          }                 // user to not be returned from findOne query
-          ],
+          required: false // fix issue where above where clause caused
+        }                 // user to not be returned from findOne query
+        ],
         order: [
-            [
-              {model: Node, as:'nodes'},
-              'id'
-            ],
-            [
-              {model: Notification, as:'notifications'},
-              'id'
-            ]
+          [
+              {model: Node, as: 'nodes'},
+            'id'
+          ],
+          [
+              {model: Notification, as: 'notifications'},
+            'id'
+          ]
         ]
       })
       .then(user => {
         if (!user) {
           return res.status(404).send({
-            message: 'Invalid login credentials provided',
-          });
+            message: 'Invalid login credentials provided'
+          })
         }
         user.password = null // don't send password back to client
         res.status(200).send(user)
       })
-      .catch((error) => res.status(400).send(error));
+      .catch((error) => res.status(400).send(error))
   },
-  
-  validate(req, res, next) {
+
+  validate (req, res, next) {
     return User
-      .findOne( { where: 
-        { 
-          api_token: req.body.api_token || req.headers.authorization
-        },
+      .findOne({ where:
+      {
+        api_token: req.body.api_token || req.headers.authorization
+      },
         include: [{
           model: Node,
-          as: 'nodes',
+          as: 'nodes'
         }],
         order: [
-            [
-              {model: Node, as:'nodes'},
-              'id'
-            ]
+          [
+              {model: Node, as: 'nodes'},
+            'id'
+          ]
         ]
       })
       .then(user => {
         if (!user) {
           return res.status(404).send({
-            message: 'Invalid API token provided',
-          });
+            message: 'Invalid API token provided'
+          })
         }
-        //console.log(user)
+        // console.log(user)
+        req.user = user
+        return next()
+      })
+      .catch(next)
+  },
+
+  validateLogin (req, res, next) {
+    return User
+      .findOne({ where:
+        { api_token: req.cookies.token },
+        include: [{ model: Node, as: 'nodes' }],
+        order: [[{model: Node, as: 'nodes'}, 'id']]
+      })
+      .then(user => {
         req.user = user
         return next()
       })
       .catch(next)
   }
-  
-};
+
+}
